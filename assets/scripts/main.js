d840fd75201ca430fef6a478b475708eac79513a
Robert Sargent <blueshiftwebdev@icloud.com>
2023-06-04 16:27:15 -0700

    finished email form settings for lets connect, added cookie banner and functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,4 +2,24 @@ let navMenu = document.getElementById("navMenu")
 
 const toggleNav = () => {
     navMenu.hidden? navMenu.removeAttribute("hidden"): navMenu.hidden="true"
+
+}
+let cookiesApproved = false;
+let cookiesDenied = false;
+
+const setCookies = (bool) => {
+    if(bool === true){
+        document.getElementById("cookiePopup").style.visibility="hidden"
+        return cookiesApproved = true;
+    }else{
+        document.getElementById("cookiePopup").style.visibility="hidden"
+        return cookiesDenied = false;
+    }
+}
+window.onload = () => {
+  if(!cookiesApproved && !cookiesDenied){
+  document.getElementById("cookiePopup").style.visibility="visible"
+  }else{
+    document.getElementById("cookiePopup").style.visibility="hidden"
+  }
 }
