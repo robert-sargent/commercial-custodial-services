7919f120124d1dc8ff3c423c9b5b80627ad1e484
Owen T Penman <openman@banyanlabs.io>
2023-06-04 17:59:04 -0700

    ShloopyBob: community impact up for review

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,34 @@
-let navMenu = document.getElementById("navMenu")
+let navMenu = document.getElementById("navMenu");
 
 const toggleNav = () => {
-    navMenu.hidden? navMenu.removeAttribute("hidden"): navMenu.hidden="true"
+  navMenu.hidden
+    ? navMenu.removeAttribute("hidden")
+    : (navMenu.hidden = "true");
+};
+function formatPhoneNumber(input) {
+  if (!input) {
+    return; // Exit the function if the input is not defined
+  }
+
+  let phoneNumber = input.value.replace(/\D/g, "");
+  let formattedNumber = "";
+
+  if (phoneNumber.length >= 3) {
+    formattedNumber += "(" + phoneNumber.substring(0, 3) + ")";
+  }
+
+  if (phoneNumber.length > 3) {
+    formattedNumber += " " + phoneNumber.substring(3, 6);
+  }
+
+  if (phoneNumber.length > 6) {
+    formattedNumber += "-" + phoneNumber.substring(6, 10);
+  }
+
+  input.value = formattedNumber;
 }
+
+const inputField = document.getElementById("phoneNumberInput");
+inputField.addEventListener("change", function (event) {
+  formatPhoneNumber(event.target);
+});
